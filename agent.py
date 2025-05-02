680569027da3d520d857e2cd6a0cfd1cbf3e80a6
lorenzoGrivet <lori.grivet@gmail.com>
2025-05-02 13:21:51 +0200

    correzione
    vai vai

--- agent.py
+++ agent.py
@@ -73,7 +73,7 @@ class Policy(torch.nn.Module):
 
         x_critic = self.tanh(self.fc1_critic(x))
         x_critic = self.tanh(self.fc2_critic(x_critic))
-        state_value = self.fc3_critic(x_critic)
+        state_value = self.fc3_critic_value(x_critic)
 
         return normal_dist, state_value
 
