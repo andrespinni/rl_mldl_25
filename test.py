edb5e22da8749334660bac864412b7d6122ad52a
andreaGaudino <andrea.gaudino02@gmail.com>
2025-07-06 10:58:25 +0200

    test ac

--- test.py
+++ test.py
@@ -33,7 +33,7 @@ def main():
     # env = gym.make('CustomHopper-target-v0')
 
     wandb.init(
-        project="ML_project",
+        project="Confronti_progetti",
         name=f"{args.name}_test",
         entity="andrea-gaudino02-politecnico-di-torino",
         config={
